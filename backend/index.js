6049ff22f7b9b2fbaf60e7fe0ba2bea41df85015
Aman Tiwari <amannitjsr86@nitjsr.ac.in>
2024-02-18 01:06:46 +0530

    after adding variable concept in css

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,6 @@ import { authMiddleware } from './middleware/auth-middleware.js';
 import { sendMail } from './controller/mail-controller.js';
 import { uploadHandler } from './controller/upload-controller.js';
 import { upload } from './Multer/multer.configuration.js';
-import { dataUpload } from './middleware/product-middleware.js';
 import { displayProduct } from './controller/displayProduct-controller.js';
 
 const app = express();
@@ -28,6 +27,32 @@ app.use(cors(corsOptions));
 const PORT = 4000;
 
 connectDB();
+
+
+// const accountSid = 'AC94ebeb0fb991e18d794ecc92ae6691ea';
+// const authToken = 'abcec0e7b55a3b242096da597e81e9f4';
+
+// const client = require('twilio')(accountSid, authToken);
+
+// client.messages
+//   .create({
+//     body: 'Hello from twilio-node',
+//     to: '+919754234746', // Text your number
+//     from: '+12345678901', // From a valid Twilio number
+//   })
+//   .then((message) => console.log(message.sid));
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- frontend/login-signup-authentication/src/routes/Routes.tsx
+++ frontend/login-signup-authentication/src/routes/Routes.tsx
@@ -1,6 +1,7 @@
 // src/components/Routes.tsx
-import React from 'react';
-import {  Route, Routes as ReactRoutes } from 'react-router-dom'; // Use a different name for the import
+import React, { useEffect, useState } from 'react';
+import {  Route, Routes as ReactRoutes , Navigate} from 'react-router-dom'; // Use a different name for the import
+// import ProtectedRoute from './ProtectedRoute.tsx';
 import Home from '../header/Home/Home';
 import About from '../header/About/About';
 import Login from '../header/login/login';
@@ -12,6 +13,16 @@ import { FileUpload } from '../header/FileUpload/FileUpload';
 import { ShowProduct } from '../header/showProduct/ShowProduct';
 
 const Routes_: React.FC = () => {
+  const ProtectedRoute = ({ path, element }: { path: string; element: React.ReactNode }) => {
+    const isAuthenticated = localStorage.getItem('token') !== null;
+    return isAuthenticated ? <Route path={path} element={element} /> : <Navigate to="/login" replace />;
+  };
+  
+  var [restricted , setRestricted] = useState<Boolean>(false);
+  
+  useEffect(()=>{
+    restricted = localStorage.getItem('token') != null;
+  } , [])
   return (
 
       <ReactRoutes> {/* Use the imported name here */}
@@ -19,10 +30,24 @@ const Routes_: React.FC = () => {
         <Route path="/about" element={<About />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/showProduct" element={<ShowProduct />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/fileUpload" element={<FileUpload />} />
-        <Route path="/showProduct" element={<ShowProduct />} />
         <Route path="/logout" element={<Logout />} />
+
+        {/* <Route path="/showProduct" element={<ShowProduct />} /> */}
+        {/* <Route path="/showProduct"
+          element={<ProtectedRoute path="/showProduct" element={<ShowProduct />} />} 
+        />
+        <Route path="/profile"
+          element={<ProtectedRoute path="/profile" element={<Profile />} />} 
+        />
+        <Route path="/fileUpload"
+          element={<ProtectedRoute path="/fileUpload" element={<FileUpload />} />} 
+        />
+        <Route path="/logout"
+          element={<ProtectedRoute path="/logout" element={<Logout />} />} 
+        /> */}
         <Route path="/forgetPassword" element={<Forget_Password />} />
 
       </ReactRoutes> 
