c22fd516da543f65832595a3c5d20c348765cd7c
Aman Tiwari <amannitjsr86@nitjsr.ac.in>
2024-02-18 20:19:27 +0530

    applied toggle button features

--- frontend/login-signup-authentication/src/navbar/Navbar.tsx
+++ frontend/login-signup-authentication/src/navbar/Navbar.tsx
@@ -6,15 +6,31 @@ import './Navbar.style.css';
 const Navbar: React.FC = () => {
   const navigate =useNavigate()
   const[token,setToken]=useState<string | null>(null)
+
+
+  const [darkMode, setDarkMode] = useState<boolean>(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
   const logout= ()=>{
     localStorage.removeItem('token');
     console.log("current Token is : ")
     navigate('/login')
   }
 
+  const rootStyles = document.documentElement.style;
+
+  useEffect(() => {
+    // Dynamically set CSS variables based on dark mode
+    rootStyles.setProperty('--background-color', darkMode ? '#9290C3' : '#97BC62');
+    rootStyles.setProperty('--header-color', darkMode ? '#535C91' : '#2C5F2D');
+  }, [darkMode]);
+
   const authToken = localStorage.getItem("token")
   return (
-    <nav className="navbar">
+    <nav className={`navbar ${darkMode ? 'dark-mode' : ''}`}>
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
@@ -33,6 +49,12 @@ const Navbar: React.FC = () => {
               <li><Link to="/login">Login</Link></li>
             </>
         }
+
+
+        <li className="toggle-btn" onClick={toggleDarkMode}>
+          {darkMode ? 'Light Mode' : 'Dark Mode'}
+        </li>
+
       </ul>
     </nav>
   );
